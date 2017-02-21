810b2aa707f8b7d183e6fb1ad79522c05fcf51e8
Ranbir Aulakh <ranbir2010@live.com>
2017-02-21 12:31:25 -0500

    Comments, revised hexToNibble function

--- getCryptoGrade.py
+++ getCryptoGrade.py
@@ -1,22 +1,32 @@
 # author: ranbir aulakh
 
 from bs4 import BeautifulSoup
-import requests
+import requests, sys
 
 def decrypt(key, ciphertext):
+	"""
+	Decrypt with provided ciphertext and key
+	Prints out your grade and other important information
+	"""
+
+	# get key
 	key = hexToByteArray(key)
 	if(key == None or len(key) != 8):
 		print("Key is empty... Or more than 8")
 		return
 
+	# get ciphertext
 	ct = hexToByteArray(ciphertext)
 	if(ct == None or len(ct) < 24):
 		print("ct is empty... Or less than 24")
 		return
 
+	# Get nonce (first 8 bytes of ciphertext), append key.
 	nonce = ct[0:8]
 	noncePlusKey = nonce + key
 
+
+	# Initialize RC4 keystream generator.
 	S = [None] * 256
 	for i in range(0, 256):
 		S[i] = i
@@ -32,6 +42,7 @@ def decrypt(key, ciphertext):
 	y = 0
 
 
+	# Decrypt ciphertext, omitting nonce.
 	for i in range(8, len(ct)):
 		x = (x + 1) & 255
 
@@ -43,88 +54,93 @@ def decrypt(key, ciphertext):
 
 		ct[i] ^= S[(S[x] + S[y]) & 255]
 
+
+	# Check key verification string.
 	for i in range(8, 24, 2):
 		if(ct[i] != ct[i+1]):
 			print("wrong key")
 			return
 
+	# display plaintext
 	print((byteArrayToString(ct[24:len(ct)])).encode('utf-8'))
 
+
 def hexToByteArray(s):
+	"""
+	Converts Hex to Byte Array
+	"""
 	if( (len(s) % 2 ) != 0):
 		return None
+
 	length = len(s) / 2
 	buf = [None] * length
+
 	for i in range(0, length):
 		x = hexToNibble( s[2*i] )
 		if(x == -1):
 			return None
+
 		y = hexToNibble( s[2*i+1] )
 		if(y == -1):
 			return None
+
 		buf[i] = (x << 4) | y
 
 	return buf
 
 def byteArrayToString(buf):
+	"""
+	Converts Byte Array To String
+	"""
 	s = "";
 	for i in range(0, len(buf)):
 		s += ''.join(map(unichr, [buf[i]]))
 	return s
 
 def hexToNibble(c):
-	u = c.upper()
-	if(u == "0"):
-		return 0
-	elif (u == "1"):
-		return 1
-	elif (u == "2"):
-		return 2
-	elif (u == "3"):
-		return 3
-	elif (u == "4"):
-		return 4
-	elif (u == "5"):
-		return 5
-	elif (u == "6"):
-		return 6
-	elif (u == "7"):
-		return 7
-	elif (u == "8"):
-		return 8
-	elif (u == "9"):
-		return 9
-	elif (u == "A"):
-		return 10
-	elif (u == "B"):
-		return 11
-	elif (u == "C"):
-		return 12
-	elif (u == "D"):
-		return 13
-	elif (u == "E"):
-		return 14
-	elif (u == "F"):
-		return 15
+	"""
+	Converts Hex to Nibble
+	"""
+	u = c.upper().strip()
+	dic = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"A":10,"B":11,"C":12,"D":13,"E":14,"F":15}
+	if(u in dic.keys()):
+		return int(dic[u])
 	else:
 		return -1
 
 def main():
+	"""
+	1. Gets URL, userId, and key
+	2. Check if page is LIVE, then get ciphertext
+	3. Execute decrypt function
+	"""
+
 	# go to https://www.cs.rit.edu/~ark/462/encryptedgrades.shtml and find your username,
 	# click on it and in the URL, you will see your userID
 	userId = "YOUR USER ID GOES HERE" 
 	url = r"https://www.cs.rit.edu/~ark/462/grades/grades.php?user=" + str(userId)
 	key = "YOUR KEY GOES HERE"
 
+	# check if website is live and running
+	try:
+		r = requests.get(url)
+	except requests.exceptions.RequestException as e:  # website is not live
+		print("Webpage not avaiable. Cannot fetch ciphertext. Please check if the website is running and you are connected to the Internet")
+		print e
+		sys.exit(1)
+
+
+	# go to URL
 	r  = requests.get(url)
 
 	data = r.text
 
 	soup = BeautifulSoup(data, "html.parser")
 
+	# get ciphertext
 	ciphertext = soup.find('input', {"name": "Ciphertext"})['value']
 
-	decrypt(key, ciphertext)
+	decrypt(key, ciphertext) # go to decrypt function
 
 main()
 
